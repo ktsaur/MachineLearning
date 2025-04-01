45957340cbdc16017f209e36b7f16856b6cc3083
ktsaur <taryshkinakv@gmail.com>
2025-04-01 15:51:58 +0300

    first homework 01.04

--- homework1.py
+++ homework1.py
@@ -10,38 +10,38 @@ def euclidean_distance(x_vect, m_vect):
     return np.linalg.norm(x_vect - m_vect)
 
 
-def plot_clusters(data, clusters, centroids, iteration):
-    COLORS = ('green', 'blue', 'brown', 'black')
-    plt.figure(figsize=(6, 5))
+def plot_clusters(clusters, centroids, iteration):
+    COLORS = ('green', 'blue', 'brown',)
+    plt.figure(figsize=(6, 5)) #создаем график и задаем параметры фигуры
 
-    for i, points in enumerate(clusters):
+    for i, points in enumerate(clusters): #i - индекс кластера, points - точки кластера
         points = np.array(points)
         if len(points) > 0:
             plt.scatter(points[:, 0], points[:, 1], s=10, color=COLORS[i])
 
-    mx = [m[0] for m in centroids]
-    my = [m[1] for m in centroids]
-    plt.scatter(mx, my, s=100, color='red', marker='x', label='Centroids')
+    mx = [m[0] for m in centroids] #по иксу все центроиды
+    my = [m[1] for m in centroids] #по игреку все центроиды
+    plt.scatter(mx, my, s=100, color='red', marker='x', label='Centroids') #рисуем центроиды на графике
 
-    plt.title(f'Iteration {iteration}')
-    plt.legend()
+    plt.title(f'Иттерация {iteration}')
     plt.show()
 
 
 def main():
     # Шаг 1. Определение оптимального числа кластеров
+    # метод локтя: строим график зависимости (суммы квадратов расстояний между объектами и их центроидом) от количества кластеров
     irises = load_iris()
     data = irises.data[:, :2]
     sse = []
 
     for i in range(1, 11):
-        kmeans = KMeans(n_clusters=i, n_init=10)
+        kmeans = KMeans(n_clusters=i, n_init=10) #n_init - указываем, что алгоритм будет выполняться 10 раз
         kmeans.fit(data)
         sse.append(kmeans.inertia_)
 
     plt.plot(range(1, 11), sse, marker='o')
     plt.xlabel('Количество кластеров')
-    plt.ylabel('Сумма квадратов расстояний (SSE)')
+    plt.ylabel('Сумма квадратов расстояний')
     plt.title('Метод локтя')
     plt.show()
 
@@ -49,36 +49,37 @@ def main():
     optimal_k = 3
 
     # Шаг 2. Алгоритм K-Means
-    centroids = [data[i] for i in np.random.choice(len(data), optimal_k, replace=False)]
+    centroids = [data[i] for i in np.random.choice(len(data), optimal_k, replace=False)] # выбираем случайные начальные центроиды
 
     iteration = 0
     while iteration < 10:
-        clusters = [[] for _ in range(optimal_k)]
+        clusters = [[] for _ in range(optimal_k)] # инициализирую пустые списки для каждого центроида
 
         # Назначение точек к ближайшему центроиду
         for x_vect in data:
             distances = [euclidean_distance(x_vect, centroid) for centroid in centroids]
-            cluster_idx = np.argmin(distances)
-            clusters[cluster_idx].append(x_vect)
+            cluster_idx = np.argmin(distances) # выбираю индекс минимального расстояния
+            clusters[cluster_idx].append(x_vect) # добавляю точку в соотвествующий кластер
 
-        prev_centroids = centroids.copy()
+        prev_centroids = centroids.copy() # копируем старые центроиды чтобы потом их сравнивать с новыми
         centroids = [np.mean(cluster, axis=0) if len(cluster) > 0 else prev_centroids[i] for i, cluster in
                      enumerate(clusters)]
+        #вычисляю новый уентроид для кластера , среднее всех точек кластера
 
 
-        plot_clusters(data, clusters, centroids, iteration + 1)
+        plot_clusters(clusters, centroids, iteration + 1)
 
         iteration += 1
 
     # Шаг 3. Финальная визуализация всех проекций
     labels = np.zeros(len(data), dtype=int)
-    for i, cluster in enumerate(clusters):
+    for i, cluster in enumerate(clusters): # присваимваем каждой точке соотвествующий кластер
         for point in cluster:
             labels[np.where((data == point).all(axis=1))[0][0]] = i
 
     feature_combinations = list(combinations(range(irises.data.shape[1]), 2))
     fig, axes = plt.subplots(2, 3, figsize=(15, 10))
-    COLORS = ('green', 'blue', 'brown', 'black')
+    COLORS = ('green', 'blue', 'brown')
 
     for ax, (i, j) in zip(axes.flatten(), feature_combinations):
         for cluster_idx in range(optimal_k):
